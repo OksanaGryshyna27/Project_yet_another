9f59133ee417b0ad78d1a00d25b18a42391f8ad6
Oksana Gryshyna <oksana.gryshyna@digitalturbine.com>
2026-02-11 18:00:22 +0100

    Fix hardcoded URL in auth setup

--- tests/auth-setup.spec.ts
+++ tests/auth-setup.spec.ts
@@ -2,21 +2,20 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pageObjects/login.page';
 import { AccountPage } from '../pageObjects/account.page'; 
 import path from 'path';
+import { USER_EMAIL, USER_PASSWORD, BASE_URL } from '../config/test-data';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-const email = process.env.USER_EMAIL!;
-const password = process.env.USER_PASSWORD!;
 
 test('has title', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await page.goto('/auth/login');
-    await loginPage.performLogin(email, password);
+    await loginPage.performLogin(USER_EMAIL, USER_PASSWORD);
    
     const accountPage = new AccountPage(page);
     
 
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+    await expect(page).toHaveURL(`${BASE_URL}/account`);
     await expect (accountPage.pageTitle).toHaveText('My account');
     await expect(accountPage.header.navMenu).toHaveText('Jane Doe')
 
