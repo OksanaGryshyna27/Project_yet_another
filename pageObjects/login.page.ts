1a33367e57813b790185b2b094317d5fe03787d7
Oksana Gryshyna <oksana.gryshyna@digitalturbine.com>
2026-02-12 13:16:43 +0100

    Fix auth-setup7

--- pageObjects/login.page.ts
+++ pageObjects/login.page.ts
@@ -17,6 +17,7 @@ export class LoginPage {
     }
 
     async performLogin(email: string, password: string): Promise<void> {
+        await this.emailField.waitFor({ state: 'visible' });
         await this.emailField.fill(email);
         await this.passwordField.fill(password);
         await this.page.getByTestId('login-submit').click();
