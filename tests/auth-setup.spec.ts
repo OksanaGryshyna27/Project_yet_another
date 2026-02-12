94445d14192e09b10b6b125a8b5c9bdbe9fb0532
Oksana Gryshyna <oksana.gryshyna@digitalturbine.com>
2026-02-12 11:32:16 +0100

    Fix auth-setup

--- tests/auth-setup.spec.ts
+++ tests/auth-setup.spec.ts
@@ -2,21 +2,22 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pageObjects/login.page';
 import { AccountPage } from '../pageObjects/account.page'; 
 import path from 'path';
+import { BASE_URL, USER_EMAIL, USER_PASSWORD } from '../config/baseConfig';
 
 const authFile = path.join(__dirname, '../playwright/.auth/user.json');
 
-const email = process.env.USER_EMAIL!;
-const password = process.env.USER_PASSWORD!;
+//const email = process.env.USER_EMAIL!;
+//const password = process.env.USER_PASSWORD!;
 
 test('has title', async ({ page }) => {
     const loginPage = new LoginPage(page);
     await page.goto('/auth/login');
-    await loginPage.performLogin(email, password);
+    await loginPage.performLogin(USER_EMAIL, USER_PASSWORD);
    
     const accountPage = new AccountPage(page);
     
 
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+    await expect(page).toHaveURL(`${BASE_URL}/account`);
     await expect (accountPage.pageTitle).toHaveText('My account');
     await expect(accountPage.header.navMenu).toHaveText('Jane Doe')
 
