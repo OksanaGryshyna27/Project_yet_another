c5905ae615e29ba07e25563ee00d41bb27c5542d
Oksana Gryshyna <oksana.gryshyna@digitalturbine.com>
2026-02-12 21:19:08 +0100

    Add API_BASE_URL to secrets

--- config/baseConfig.ts
+++ config/baseConfig.ts
@@ -6,15 +6,5 @@ dotenvConfig({ path: join(process.cwd(), '.env') });
 export const BASE_URL: string = process.env.BASE_URL ?? 'https://practicesoftwaretesting.com';
 export const API_BASE_URL: string = process.env.API_BASE_URL ?? 'https://api.practicesoftwaretesting.com';
 
-function requireEnv(name: string): string {
-  const value = process.env[name];
-
-  if (!value) {
-    throw new Error(`Environment variable ${name} is not defined`);
-  }
-
-  return value;
-}
-
-export const USER_EMAIL: string = requireEnv('USER_EMAIL');
-export const USER_PASSWORD: string = requireEnv('USER_PASSWORD');
+export const USER_EMAIL: string = process.env.USER_EMAIL ?? 'customer@practicesoftwaretesting.com';
+export const USER_PASSWORD: string = process.env.USER_PASSWORD ?? 'welcome01';
